f0c20ee962b7a6177fddb68661becadd8f0fd6e0
David Matterne <david.matterne@gmail.com>
2016-09-28 10:57:41 +0200

    Initial Commit

---
+++ Memory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Memory
+//
+//  Created by Stannis Baratheon on 28/09/16.
+//  Copyright © 2016 Training. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
