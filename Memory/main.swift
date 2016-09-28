c72ba8c50fe61439cd2cd9f5c9c72b61472844a7
David Matterne <david.matterne@gmail.com>
2016-09-28 11:12:29 +0200

    Final

--- Memory/main.swift
+++ Memory/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!") 
+print("Hello, World!")
+
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialised")
+    }
+    deinit {
+        print("\(name) is being deinitialised")
+    } }
+
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
+//weak var reference3: Person?
+
+reference1 = Person(name: "Luke")
+reference2 = reference1
+reference3 = reference1
+
+reference1 = nil
+reference2 = nil
+//reference3 = nil
 
